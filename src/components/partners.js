ac91982a0b7bb371d9b6527f075f780b4ce7cb0b
ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2024-06-09 00:56:53 -0400

    scrolling sponsors

--- src/components/partners.js
+++ src/components/partners.js
@@ -6,18 +6,9 @@ import { motion } from 'framer-motion';
 
 export default function Partners() {
 	const { width, height } = useWindowSize();
-	return (
-		<>
-			<motion.div
-				className="flex justify-center items-center flex-row gap-10"
-				animate={[{ translateX: 0 }, { translateX: -420 }, { translateX: -840 }]}
-				transition={{
-					repeat: Infinity,
-					repeatType: 'reverse',
-					ease: 'linear',
-					duration: 20,
-				}}
-			>
+	if (width > 640) {
+		return (
+			<div className="flex justify-center items-center flex-row gap-10">
 				<Image src="/partners/xero-logo.svg" width={75} height={75} alt="Xero" />
 				<Image src="/partners/expensify-logo.svg" width={200} height={100} alt="Quickbooks" />
 				<Image src="/partners/bill.com-logo.svg" width={200} height={100} alt="Bill.com" />
@@ -28,7 +19,34 @@ export default function Partners() {
 					height={100}
 					alt="Quickbooks"
 				/>
-			</motion.div>
-		</>
-	);
+			</div>
+		);
+	} else {
+		return (
+			<>
+				<motion.div
+					className="flex justify-center items-center flex-row gap-10"
+					animate={[{ translateX: 0 }, { translateX: -350 }]}
+					initial={{ translateX: 350 }}
+					transition={{
+						repeat: Infinity,
+						repeatType: 'reverse',
+						ease: 'linear',
+						duration: 10,
+					}}
+				>
+					<Image src="/partners/xero-logo.svg" width={75} height={75} alt="Xero" />
+					<Image src="/partners/expensify-logo.svg" width={200} height={100} alt="Quickbooks" />
+					<Image src="/partners/bill.com-logo.svg" width={200} height={100} alt="Bill.com" />
+					<Image src="/partners/tsheets-logo.svg" width={200} height={100} alt="T-Sheets" />
+					<Image
+						src="/partners/intuit-quickbooks-logo.svg"
+						width={200}
+						height={100}
+						alt="Quickbooks"
+					/>
+				</motion.div>
+			</>
+		);
+	}
 }
