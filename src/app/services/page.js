fdfd7bbe038d3fa1bf406a047a15face07dc132b
ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2025-01-18 00:54:09 -0500

    New services page

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -38,48 +38,124 @@ export default function ServicesPage() {
 					<div className="grid auto-rows-auto sm:grid-cols-3 gap-5 mt-5">
 						<div className="flex flex-col p-10 bg-white mr-10 rounded-3xl w-full drop-shadow-2xl">
 							<h2>Starter</h2>
+							<h3 className="font-bold">$249/mo</h3>
+							<div className="my-3"></div>
 							<ul className="list-disc">
-								<li>Account set up and monthly subscription in QBO</li>
-								<li>Up to 3 bank/credit card accounts</li>
-								<li>Up to 100 transactions</li>
-								<li>Monthly account reconciliation</li>
-								<li>3 monthly financial reports</li>
+								<li>
+									Monthly recording, classifying and reconciling of up to 3 bank/credit card
+									accounts
+								</li>
+								<li>Up to 200 transactions</li>
+								<li>Monthly financial reports</li>
 								<li>Unlimited email support</li>
+								<li>No contract (30 days notice required to cancel)</li>
 							</ul>
+							<div className="mt-auto">
+								<GetInTouch />
+							</div>
 						</div>
 						<div className="flex flex-col p-10 bg-white mr-10 rounded-3xl w-full drop-shadow-2xl">
 							<h2>Standard</h2>
+							<h3 className="font-bold">$549/mo</h3>
+							<div className="my-3"></div>
 							<ul className="list-disc">
-								<li>Account set up and monthly subscription in QBO</li>
-								<li>Up to 4 bank/credit card accounts</li>
-								<li>Up to 200 transactions</li>
-								<li>Monthly account reconciliation</li>
-								<li>3 monthly financial reports</li>
+								<li>
+									Monthly recording, classifying and reconciling of up to 5 bank/credit card
+									accounts
+								</li>
+								<li>Up to 350 transactions</li>
+								<li>Monthly financial reports</li>
 								<li>Unlimited email support</li>
+								<li>No contract (30 days notice required to cancel)</li>
 							</ul>
+							<div className="mt-auto">
+								<GetInTouch />
+							</div>
 						</div>
 						<div className="flex flex-col p-10 bg-white mr-10 rounded-3xl w-full drop-shadow-2xl">
 							<h2>Premium</h2>
+							<h3 className="font-bold">$979/mo</h3>
+							<div className="my-3"></div>
 							<ul className="list-disc">
-								<li>Account set up and monthly subscription in QBO</li>
-								<li>Up to 6 bank/credit card accounts</li>
-								<li>Monthly account reconciliation</li>
-								<li>3 monthly financial reports</li>
-								<li>Monthly live call</li>
+								<li>
+									Monthly recording, classifying and reconciling of up to 8 bank/credit card
+									accounts
+								</li>
+								<li>Unlimited transactions</li>
+								<li>Monthly financial reports</li>
 								<li>Unlimited email support</li>
+								<li>No contract (30 days notice required to cancel)</li>
+								<li>A/P and A/R</li>
 							</ul>
+							<div className="mt-5">
+								<GetInTouch />
+							</div>
 						</div>
 					</div>
 				</div>
-				<div className="bg-black text-white sm:px-20 sm:py-12 px-5 py-16">
+				{/* <div className="bg-black text-white sm:px-20 sm:py-12 px-5 py-16">
 					<h2>Clean up</h2>
 					<p className="mb-5">
 						If you need to fix, update and organize your books we can help you prices vary depending
 						on how extensive the clean up is. Once done, we can help you maintain your books.
 					</p>
 					<GetInTouch />
+				</div> */}
+				<div className="bg-white text-black sm:px-20 sm:py-12 px-5 py-16  flex flex-col justify-center items-center">
+					<div className="w-1/2 flex flex-col justify-center items-center">
+						<h2 className="underline">One Time Cleanup Service</h2>
+						<img src="/folder.png" className="my-2 invert" />
+						<p className="text-center">
+							If you need to fix, update and organize your books we can help you. <br />
+							Prices vary depending on how extensive the clean up is. Once done, we can help you
+							maintain your books.
+						</p>
+					</div>
+					<h2 className="mt-16 underline">Monthly Recurring Service</h2>
+					<div className="flex flex-row justify-center items-start gap-24 mt-2 mb-16">
+						<div>
+							<div className="flex justify-center items-center">
+								<img src="/folder_switch.png" className="my-2 invert" />
+							</div>
+
+							<ul className="list-disc">
+								<li>Customize Chart of Accounts</li>
+								<li>Reconcile Account Balances</li>
+							</ul>
+						</div>
+
+						<div>
+							<div className="flex justify-center items-center">
+								<img src="/payroll.png" className="my-2 invert" />
+							</div>
+							<ul className="list-disc">
+								<li>Accounts Payable</li>
+								<li>Accounts Receivable</li>
+								<li>Payroll</li>
+							</ul>
+						</div>
+						<div>
+							<div className="flex justify-center items-center">
+								<img src="/book.png" className="my-2 invert" />
+							</div>
+							<ul className="list-disc">
+								<li>Record Transactions</li>
+								<li>Adjusting Journal Entries</li>
+							</ul>
+						</div>
+						<div>
+							<div className="flex justify-center items-center">
+								<img src="/statement.png" className="my-2 invert" />
+							</div>
+							<ul className="list-disc">
+								<li>Financial Statements</li>
+								<li>Advisory</li>
+							</ul>
+						</div>
+					</div>
+					<GetInTouch />
 				</div>
-				<div className="bg-white text-black sm:px-20 sm:py-12 px-5 py-16">
+				<div className="bg-black text-white sm:px-20 sm:py-12 px-5 py-16">
 					<h2>Start up</h2>
 					<p className="mb-5">
 						Don&apos;t have a QBO account and don&apos;t know where to start? We can help you set up
