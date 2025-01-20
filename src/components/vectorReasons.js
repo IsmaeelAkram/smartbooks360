0975fb3ebf59dfcad4d6d9f49e93cb8d0d729ebe
ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2025-01-19 21:12:30 -0500

    mobile vectorReason

--- src/components/vectorReasons.js
+++ src/components/vectorReasons.js
@@ -39,18 +39,18 @@ export default function VectorReasons() {
 							<p>
 								We understand how frustrating it is to feel like your bookkeeper or accountant is
 								too busy for you. At SmartBooks360, we pride ourselves on prompt, clear
-								communication—you’ll never feel left in the dark.
+								communication—you&apos;ll never feel left in the dark.
 							</p>
 						</div>
 					</>
 				) : (
 					<>
 						<div className="sm:col-span-4 flex justify-center items-start flex-col mx-10">
-							<h2>Stress-free and effortless</h2>
+							<h2>Tired of Being Ignored by Your Bookkeeper?</h2>
 							<p>
-								Get rid of the frustrations and cash surprises that come with messy books. Let our
-								team of expert outsourced accountants give you accurate and timely financial
-								information about your small business.
+								We understand how frustrating it is to feel like your bookkeeper or accountant is
+								too busy for you. At SmartBooks360, we pride ourselves on prompt, clear
+								communication—you&apos;ll never feel left in the dark.
 							</p>
 						</div>
 						<div className="col-span-4 sm:col-span-2 flex justify-center items-center">
